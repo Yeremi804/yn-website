a49b27dc0607704a12b3d7178e65ac8e9fd5402b
Yeremi Navarrete <yereminavarrete03@gmail.com>
2026-06-15 17:44:30 -0700

    updated to previous working github

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 
 import Home from './pages/Home';
@@ -6,20 +6,18 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import NoMatch from './pages/NoMatch';
 import Project from './pages/Project';
-;
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path ="/" element={<Home />} />
-          <Route path ="/about" element={<About />} />
-          <Route path ="/contact" element={<Contact />} />
-          <Route path ="/project" element={<Project />} />
-          <Route path ="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
