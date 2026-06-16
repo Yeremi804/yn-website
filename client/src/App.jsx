f2f32d63a06b67cb7a3eb1a31c9d25f657f555eb
Yeremi Navarrete <yereminavarrete03@gmail.com>
2026-06-15 17:07:03 -0700

    Update minor bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NoMatch from "./pages/NoMatch";
-import Project from "./pages/Project";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NoMatch from './pages/NoMatch';
+import Project from './pages/Project';
+;
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="*" element={<NoMatch />} />
+          <Route path ="/" element={<Home />} />
+          <Route path ="/about" element={<About />} />
+          <Route path ="/contact" element={<Contact />} />
+          <Route path ="/project" element={<Project />} />
+          <Route path ="*" element={<NoMatch />} />
         </Route>
       </Routes>
     </Router>
